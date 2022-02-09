5f0818108d4380d37a67ae94c5606654c7bccf4b
Andrey <aamancio74@gmail.com>
2022-02-09 20:05:10 -0300

    Tarefa de Media de notas

---
+++ mod8/Media/src/MediaNotas.java
@@ -0,0 +1,17 @@
+public class MediaNotas{
+    public static void main (String args[]){
+
+        double aluno1 = 8.9;
+        double aluno2 = 7.1;
+        double aluno3 = 4.2;
+        double aluno4 = 3.2;
+
+        double media = aluno1 + aluno2 + aluno3 + aluno4;
+        double mediaTotal = media /4;
+        System.out.print(mediaTotal);
+    }
+}
+
+
+
+
