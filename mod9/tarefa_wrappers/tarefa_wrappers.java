41776443131adf6e2f9f0ee234ace94e23a641b2
Andrey <aamancio74@gmail.com>
2022-02-18 15:56:57 -0300

    tarefa wrappers

---
+++ mod9/tarefa_wrappers/tarefa_wrappers.java
@@ -0,0 +1,10 @@
+public class tarefa_wrappers {
+
+    public static void main (String args[]) {
+
+
+        int codigo = 3999;
+        Integer codigo1 = codigo;
+        System.out.println(codigo);
+    }
+}
